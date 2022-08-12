e1bc56f73db19245378635ae099086b3c186dab7
MaximMukhin <eriwer@gmail.com>
2022-08-12 21:35:10 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 import "./styles.css";
+
+const form = document.getElementById("form");
+const input = form.querySelector("#task-input");
+const btnAddTask = form.querySelector("#btn-add-task");
+
+const addTask = () => {};
+
+form.addEventListener("click", addTask);
+
 console.log("app working");

e87649818c3c9c6457b1708e79a2ac5840e05ccc
MaximMukhin <eriwer@gmail.com>
2022-08-13 16:18:12 +0300

    инпут

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
+import isValid from "./module/isValid";
 import "./styles.css";
 
 const form = document.getElementById("form");
 const input = form.querySelector("#task-input");
 const btnAddTask = form.querySelector("#btn-add-task");
 
+console.log(isValid(input.value));
+
 const addTask = (event) => {
   event.preventDefault();
 
-  console.log(input.value);
+  if (isValid(input.value)) {
+    const task = {
+      text: input.value.trim(),
+      boolean: false,
+      date: new Date().toJSON(),
+    };
+
+    btnAddTask.disabled = true;
+
+    console.log("task", task);
+    setTimeout(() => {
+      input.value = "";
+      input.className = "";
+    }, 1000);
+  }
 };
 
-form.addEventListener("click", addTask);
-
-
-console.log("app working");
+btnAddTask.addEventListener("click", addTask);
+input.addEventListener("input", () => {
+  btnAddTask.disabled = !isValid(input.value);
+});

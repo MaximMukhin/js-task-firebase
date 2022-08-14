24f5dace33916a837c538826debc0c09d5ece5a8
MaximMukhin <eriwer@gmail.com>
2022-08-14 19:54:23 +0300

    fix

--- src/task.js
+++ src/task.js
@@ -20,6 +20,12 @@ export class Task {
   }
   static renderTaskList() {
     const task = getTaskFromLocalStorage();
+    const html = task.length
+      ? task.map(renderOneTask).join("")
+      : `<div class="mui--text-headline">Нет задач</div>`;
+
+    const list = document.querySelector("#render-list");
+    list.innerHTML = html;
   }
 }
 
@@ -31,4 +37,15 @@ function addToLocalStorage(task) {
 
 function getTaskFromLocalStorage() {
   return JSON.parse(localStorage.getItem("task") || "[]");
+}
+
+function renderOneTask(task) {
+  return `
+  <div class="mui--text-black-54"> Создана:
+  ${new Date(task.date).toLocaleDateString()}
+  ${new Date(task.date).toLocaleTimeString()}
+  </div>
+  <div>${task.text}</div>
+  <br>
+  `;
 }

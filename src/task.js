a428234f059a04021103ae62990f64e7aa0954c7
MaximMukhin <eriwer@gmail.com>
2022-08-14 18:45:20 +0300

    task fix

--- src/task.js
+++ src/task.js
@@ -1,1 +1,18 @@
-export class task {}
+export class task {
+  static create(task) {
+    return fetch(
+      "https://js-authorization-aac72-default-rtdb.firebaseio.com/task.json",
+      {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      });
+  }
+}

54f5d56871c9fa57889a9f74bce5206adc37c0b2
MaximMukhin <eriwer@gmail.com>
2022-08-15 20:47:44 +0300

    add test

---
+++ src/test.js
@@ -0,0 +1,26 @@
+const obj = {
+  "task": {
+      "-N9XP7UI4XlAXleswitw": {
+          "boolean": false,
+          "date": "2022-08-15T16:48:54.686Z",
+          "text": "Настроить 1с унф"
+      },
+      "-N9XPD4hYwOTsE10I6cw": {
+          "boolean": false,
+          "date": "2022-08-15T16:49:17.582Z",
+          "text": "Оформить новый заказ АС-09"
+      },
+      "-N9XPLMhy2mLYoCiuSQK": {
+          "boolean": false,
+          "date": "2022-08-15T16:49:51.511Z",
+          "text": "Настроить сервер"
+      },
+      "-N9XQMNvSrAd2DNV8tia": {
+          "boolean": false,
+          "date": "2022-08-15T16:54:17.516Z",
+          "text": "Починить станок чпу"
+      }
+  }
+}
+
+obj.length; //=

1be61ab1db22c91de848ce841ec31eea1aaa62ae
MaximMukhin <eriwer@gmail.com>
2022-08-15 20:41:43 +0300

    add getTasks

---
+++ src/module/getTasks.js
@@ -0,0 +1,6 @@
+const getTasks = () => {
+  return fetch("https://js-task-firebase-default-rtdb.firebaseio.com/task.json")
+    .then((response) => response.json())
+    .then((respons) => respons);
+};
+export default getTasks;

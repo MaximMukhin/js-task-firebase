b9fea60aedbaea1c2fa4fba5bf9f93ad6e25679e
MaximMukhin <eriwer@gmail.com>
2022-08-16 20:38:06 +0300

    fix

--- src/module/renderTaskList.js
+++ src/module/renderTaskList.js
@@ -1,3 +1,14 @@
-const renderTaskList = () => {};
+const renderTaskList = (coll) => {
+    coll.forEach((task) => {
+        return `
+  <div class="mui--text-black-54"> Создана:
+  ${new Date(task.date).toLocaleDateString()}
+  ${new Date(task.date).toLocaleTimeString()}
+  </div>
+  <div>${task.text} <input type="checkbox" ></input></div>
+  <br>
+  `;
+    })
+};
 
 export default renderTaskList;

51275152380397a61ecc072c103c4fddc22471c1
MaximMukhin <eriwer@gmail.com>
2022-08-18 17:04:40 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 import clg from "./module/clg.js";
 
 clg();
+const input = document.querySelector("#input-task");
+const btnGetInput = document.querySelector("#get-input");
+
+const getInput = () => {
+  console.log("Работает");
+};
+
+btnGetInput.addEventListener("click", getInput);

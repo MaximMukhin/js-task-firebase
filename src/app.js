5613e15480fc03f51fdd2acd8b58e5d1e70076f1
MaximMukhin <eriwer@gmail.com>
2022-08-19 21:04:00 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,13 +1,40 @@
 import getInput from "./module/getInput.js";
 
+const tasks = [];
+
 const btnGetInput = document.querySelector("#get-input");
-const inputText = document.querySelector("#input-text");
+// const inputText = document.querySelector("#input-text");
+const renderList = document.querySelector("#render-list");
 
 const renderText = () => {
   const textInput = getInput();
-  inputText.innerHTML = textInput;
+  tasks.push({ text: textInput, boolean: false });
+  //  inputText.innerHTML = textInput;
+  const input = document.querySelector("#input-task");
+  input.value = "";
+  console.log(tasks);
+  renderListTask();
+};
+
+btnGetInput.addEventListener("click", renderText);
+
+const renderListTask = () => {
+  let outHtml = "";
+  tasks.forEach((el) => {
+    outHtml += `
+<div>
+
+  <div>${el.text}</div>
+  <div>
+    <input type="checkbox">
+    <button>Удалить</button>
+  </div>
+
+</div>
+
+    `;
+  });
+  renderList.innerHTML = outHtml;
 };
 
 
-
-btnGetInput.addEventListener("click", renderText);

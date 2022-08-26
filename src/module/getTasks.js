4fa87c29069d39c45cababbf059bcecf1a17c9cf
MaximMukhin <eriwer@gmail.com>
2022-08-26 21:50:20 +0300

    fix

--- src/module/getTasks.js
+++ src/module/getTasks.js
@@ -3,47 +3,53 @@ const getTasks = () => {
     .then((response) => response.json())
     .then((data) => {
       const tasks = Object.entries(data);
-      const newTasks = tasks.map((el) => {
+      let newTasks = tasks.map((el) => {
         return { id: el[0], ...el[1] };
       });
+      newTasks = [];
       renderList(newTasks);
     });
 };
 
 const renderList = (arr) => {
   console.log(arr);
+
   let html = "";
 
-  arr.forEach((el) => {
-    html += `
-    <div class="flex-wrapper">
-      <div>
-        <div>      
-        ${new Date(el.date).toLocaleDateString()}
-        ${new Date(el.date).toLocaleTimeString()}
+  if (arr.length) {
+    arr.forEach((el) => {
+      html += `
+      <div class="flex-wrapper">
+        <div>
+          <div>      
+          ${new Date(el.date).toLocaleDateString()}
+          ${new Date(el.date).toLocaleTimeString()}
+          </div>
+          <div>${el.text}</div>
         </div>
-        <div>${el.text}</div>
+        <div style="display: flex; align-items: center;">
+          <input 
+          class="form-check-input" 
+          type="checkbox" 
+          id="checkboxNoLabel" 
+          value="" aria-label="..."
+          >
+          <button 
+          type="button" 
+          type="button" class="btn btn-outline-danger"
+          style="margin-left: 8px"
+          >
+          Удалить
+          </button>
+        </div>
       </div>
-      <div style="display: flex; align-items: center;">
-        <input 
-        class="form-check-input" 
-        type="checkbox" 
-        id="checkboxNoLabel" 
-        value="" aria-label="..."
-        >
-        <button 
-        type="button" 
-        type="button" class="btn btn-outline-danger"
-        style="margin-left: 8px"
-        >
-        Удалить
-        </button>
-      </div>
-    </div>
-    `;
-  });
-  const list = document.querySelector("#list");
-  list.innerHTML = html;
+      `;
+    });
+    const list = document.querySelector("#list");
+    list.innerHTML = html;
+  } else {
+    list.innerHTML = "Нет задач";
+  }
 };
 
 export default getTasks;

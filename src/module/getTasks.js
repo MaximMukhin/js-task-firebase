adb0e3f851213bec824d76e17b20dbadb8f10048
MaximMukhin <eriwer@gmail.com>
2022-08-26 21:40:27 +0300

    ref add renderList

--- src/module/getTasks.js
+++ src/module/getTasks.js
@@ -6,40 +6,44 @@ const getTasks = () => {
       const newTasks = tasks.map((el) => {
         return { id: el[0], ...el[1] };
       });
-      console.log(newTasks);
-      let html = "";
-
-      newTasks.forEach((el) => {
-        html += `
-        <div class="flex-wrapper">
-          <div>
-            <div>      
-            ${new Date(el.date).toLocaleDateString()}
-            ${new Date(el.date).toLocaleTimeString()}
-            </div>
-            <div>${el.text}</div>
-          </div>
-          <div style="display: flex; align-items: center;">
-            <input 
-            class="form-check-input" 
-            type="checkbox" 
-            id="checkboxNoLabel" 
-            value="" aria-label="..."
-            >
-            <button 
-            type="button" 
-            type="button" class="btn btn-outline-danger"
-            style="margin-left: 8px"
-            >
-            Удалить
-            </button>
-          </div>
-        </div>
-        `;
-      });
-      const list = document.querySelector("#list");
-      list.innerHTML = html;
+      renderList(newTasks);
     });
+};
+
+const renderList = (arr) => {
+  console.log(arr);
+  let html = "";
+
+  arr.forEach((el) => {
+    html += `
+    <div class="flex-wrapper">
+      <div>
+        <div>      
+        ${new Date(el.date).toLocaleDateString()}
+        ${new Date(el.date).toLocaleTimeString()}
+        </div>
+        <div>${el.text}</div>
+      </div>
+      <div style="display: flex; align-items: center;">
+        <input 
+        class="form-check-input" 
+        type="checkbox" 
+        id="checkboxNoLabel" 
+        value="" aria-label="..."
+        >
+        <button 
+        type="button" 
+        type="button" class="btn btn-outline-danger"
+        style="margin-left: 8px"
+        >
+        Удалить
+        </button>
+      </div>
+    </div>
+    `;
+  });
+  const list = document.querySelector("#list");
+  list.innerHTML = html;
 };
 
 export default getTasks;

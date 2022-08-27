acf5066ffc7205a36793a1fb79311fb5adf24693
MaximMukhin <eriwer@gmail.com>
2022-08-27 15:26:59 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,27 +1,6 @@
 import addTask from "./module/addTask.js";
 import getTasks from "./module/getTasks.js";
-// import getInput from "./module/getInput.js";
-// import renderListTask from "./module/renderListTask.js";
-// import { Task } from "./module/task.js";
 
 window.addEventListener("DOMContentLoaded", getTasks);
 
-const buttonAddTask = document.querySelector("#add-task");
-// let input = document.querySelector("#input-task");
-
-// const addTask = async () => {
-//   const textInput = getInput(input);
-//   const task = {
-//     text: textInput,
-//     completed: false,
-//     date: new Date().toJSON(),
-//   };
-
-//   await Task.create(task);
-//   await getTasks();
-
-//   input.value = "";
-//   console.log(task);
-// };
-
-buttonAddTask.addEventListener("click", addTask);
+document.querySelector("#add-task").addEventListener("click", addTask);

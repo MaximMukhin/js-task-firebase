739262e77e6d62dae64c150ab1f08016644aaecd
MaximMukhin <eriwer@gmail.com>
2022-08-28 18:28:18 +0300

    fix

--- src/module/removeTask.js
+++ src/module/removeTask.js
@@ -1,16 +1,12 @@
 const removeTask = async (id) => {
-  console.log('удалить')
-  // const res = await fetch(`https://js-task-firebase-default-rtdb.firebaseio.com/task/${id}.json`,
-  //   {
-  //     method: 'DELETE',
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     }
-  //   })
-  // const data = await res.json()
-  // console.log(data)
+  const res = await fetch(`https://js-task-firebase-default-rtdb.firebaseio.com/task/${id}.json`,
+    {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+  const data = await res.json()
 }
-
-// document.getElementById('removeTask').addEventListener('click', removeTask)
 
 export default removeTask;

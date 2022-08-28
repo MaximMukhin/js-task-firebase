0bcd840f16f7ec723dbe2f0bbfa175b2000c70f8
MaximMukhin <eriwer@gmail.com>
2022-08-28 18:28:27 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import addTask from "./module/addTask.js";
 import getTasks from "./module/getTasks.js";
-// import removeTask from "./module/removeTask.js";
+import removeTask from "./module/removeTask.js";
 
 window.addEventListener("DOMContentLoaded", getTasks);
 
 document.querySelector("#add-task").addEventListener("click", addTask);
 
-setTimeout(() => {
-  document.querySelector('.list').addEventListener('click', (event) => {
 
-    if (event.target.dataset.action === 'delete') {
-      console.log('del')
-      const parent = event.target.closest('.task-wrapper')
-      console.log(parent)
-    }
-  })
-}, 1000)
+document.querySelector('.list').addEventListener('click', (event) => {
+  if (event.target.dataset.action === 'delete') {
+    const parent = event.target.closest('.task-card')
+    parent.remove()
+    removeTask(parent.id)
+
+    console.log(parent.id)
+  }
+})
+

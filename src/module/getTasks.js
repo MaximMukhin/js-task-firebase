2b4d9b21958c0fa84d835bdaba2980972a88e0eb
MaximMukhin <eriwer@gmail.com>
2022-08-29 21:32:40 +0300

    fix

--- src/module/getTasks.js
+++ src/module/getTasks.js
@@ -1,73 +1,22 @@
+import renderTaskList from "./renderTaskList.js";
 const getTasks = () => {
   return fetch("https://js-task-firebase-default-rtdb.firebaseio.com/task.json")
     .then((response) => response.json())
     .then((data) => {
       const tasks = Object.entries(data);
       const newTasks = tasks.map((el) => {
-        return {id: el[0], ...el[1]};
+        return { id: el[0], ...el[1] };
       });
       renderTaskList(newTasks);
     })
-    .catch(e => {
-      console.log('Список пуст')
+    .catch((e) => {
+      console.log("Список пуст");
       list.innerHTML = `
       <div class="alert alert-warning" role="alert">
       Список задач пуст!
       </div>
       `;
-    })
-};
-
-const renderTaskList = (arr) => {
-  let html = "";
-
-  if (arr.length) {
-    arr.forEach(({id, completed, date, text}) => {
-      html += `
-      <div class="task-card" id="${id}">
-        <div class="task-title">
-          <div>  
-          Дата cоздания:    
-          ${new Date(date).toLocaleDateString()}
-          ${new Date(date).toLocaleTimeString()}
-          </div>
-          <br>
-          <div>${text}</div>
-        </div>
-        <div style="display: flex; align-items: center;">
-          <input 
-          class="form-check-input" 
-          type="checkbox" 
-          id="checkboxNoLabel" 
-          data-action="completed"
-          value="" aria-label="..."
-          ${completed && "checked"}
-          >
-          <button 
-          id="remove-task"
-          data-action="delete"
-          type="button" 
-          class="btn btn-outline-danger"
-          style="margin-left: 8px"
-          >
-          Удалить
-          </button>
-        </div>
-      </div>
-      `;
     });
-    const list = document.querySelector("#list");
-    list.innerHTML = html;
-  } else {
-    list.innerHTML = `
-      <div class="alert alert-warning" role="alert">
-      Список задач пуст!
-      </div>
-      `;
-  }
 };
 
 export default getTasks;
-
-
-
